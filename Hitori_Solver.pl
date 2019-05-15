3cb62788b878b1f7d090fcc09a0b5268ae70e762
Sami <Sami-esh@hotmail.com>
2019-05-15 03:21:16 +0300

    Init Hitori Grid

---
+++ Hitori_Solver.pl
@@ -0,0 +1,30 @@
+% Hitori Game Size
+size(4,4).
+
+% Fill Grid
+cell(1,1,2).
+cell(1,2,2).
+cell(1,3,2).
+cell(1,4,4).
+
+cell(2,1,1).
+cell(2,2,4).
+cell(2,3,2).
+cell(2,4,3).
+
+cell(3,1,2).
+cell(3,2,3).
+cell(3,3,2).
+cell(3,4,1).
+
+cell(4,1,3).
+cell(4,2,4).
+cell(4,3,1).
+cell(4,4,2).
+
+% Black Cells
+black(1,1).
+black(1,3).
+black(3,3).
+black(4,2).
+

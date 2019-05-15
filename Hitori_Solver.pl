a44a64138096be3db1501b5eddae656129fb9c7a
Sami <Sami-esh@hotmail.com>
2019-05-15 13:44:01 +0300

    add Method : Finding Number in row / column

--- Hitori_Solver.pl
+++ Hitori_Solver.pl
@@ -28,3 +28,26 @@ black(1,3).
 black(3,3).
 black(4,2).
 
+
+% ------------------------------------
+
+
+is_white(X,Y):- cell(X,Y,_) , not(black(X,Y)).
+
+% Finds nextNumber(N) in Row
+row_next(_,5,_):- false,!.
+row_next(X,Y,N):- cell(X,Y,Y1), ( (Y1 =\= N)-> NY is Y+1 ,row_next(X,NY,N) ; true,!).
+
+% Finds prevNumber(N) in Row
+row_prev(_,0,_):- false,!. 
+row_prev(X,Y,N):- cell(X,Y,Y1), ( (Y1 =\= N)-> NY is Y-1 ,row_prev(X,NY,N) ; true,!).
+
+% Finds nextNumber(N) in Column
+column_next(5,_,_):- false,!.
+column_next(X,Y,N):- cell(X,Y,Y1), ( (Y1 =\= N)-> NX is X+1 ,column_next(NX,Y,N) ; true,!).
+
+% Finds prevNumber(N) in Column
+column_prev(0,_,_):- false,!. 
+column_prev(X,Y,N):- cell(X,Y,Y1), ( (Y1 =\= N)-> NX is X-1 ,column_prev(NX,Y,N) ; true,!).
+
+
